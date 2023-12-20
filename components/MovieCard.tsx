29c275087c38c7679001fe1ac2337101a37e9a81
Henry Baharudin Yusuf <baharhenry10@gmail.com>
2023-12-20 18:27:07 +0700

    fake project netflix clone done

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { useRouter } from "next/router";
 
+import useInfoModal from "@/hooks/useInfoModal";
+
 import { BsFillPlayFill } from "react-icons/bs";
+import { BiChevronDown } from "react-icons/bi";
 import FavoriteButton from "./FavoriteButton";
 
 interface MovieCardProps {
@@ -10,6 +13,12 @@ interface MovieCardProps {
 
 const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter();
+
+  const { openModal } = useInfoModal();
+
+  const handleOpenModal = useCallback(() => {
+    openModal(data?.id);
+  }, [openModal, data?.id]);
 
   return (
     <div className="group bg-zinc-900 col-span-1 relative h-[12vw]">
@@ -34,6 +43,13 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
               <BsFillPlayFill size={30} />
             </div>
             <FavoriteButton movieId={data?.id} />
+            <div className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+              <BiChevronDown
+                onClick={handleOpenModal}
+                size={30}
+                className="text-white group-hover/item:text-neutral-300"
+              />
+            </div>
           </div>
           <p className="text-green-400 font-semibold mt-4">
             New <span className="text-white">2023</span>
